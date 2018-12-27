2174aa92aacfcc99bda93fef8b9aad6cbbf7fa25
Song <quanpengsong@gmail.com>
2018-12-27 22:25:52 +0800

    temporary main activity

--- app/src/main/java/com/example/song/herocalculator/MainActivity.java
+++ app/src/main/java/com/example/song/herocalculator/MainActivity.java
@@ -8,19 +8,34 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
 
     TextView descriptionTextView;
+    Item[] equipments = new Item[5];
+    Item textViewItem; // 用来汇总
+    Item item;
+    Item itemHero;
+    ImageButton heroImageButton;
+    ImageButton item1Button;
+    ImageButton item2Button;
+    ImageButton item3Button;
+    ImageButton item4Button;
+    String description;
+    ImageButton item5Button;
+    EditText searchEditText;
+    ItemFactory itemFactory;
+    ImageButton searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,32 +53,110 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-//        ItemFactory itemFactory = new ItemFactory(this);
-//        Item item = itemFactory.getItemByKeyword("23");
-//        Log.d("debug1", item.description);
-
-        ImageButton searchButton = (ImageButton) findViewById(R.id.searchBtn);
-
-
-        final ImageButton heroImageButton = (ImageButton) findViewById(R.id.heroImageBtn);
-        final ImageButton item1Button = (ImageButton) findViewById(R.id.item1ImageBtn);
-        final ImageButton item2Button = (ImageButton) findViewById(R.id.item2ImageBtn);
-        final ImageButton item3Button = (ImageButton) findViewById(R.id.item3ImageBtn);
-        final ImageButton item4Button = (ImageButton) findViewById(R.id.item4ImageBtn);
-        final ImageButton item5Button = (ImageButton) findViewById(R.id.item5ImageBtn);
+        searchButton = (ImageButton) findViewById(R.id.searchBtn);
+        heroImageButton = (ImageButton) findViewById(R.id.heroImageBtn);
+        item1Button = (ImageButton) findViewById(R.id.item1ImageBtn);
+        item2Button = (ImageButton) findViewById(R.id.item2ImageBtn);
+        item3Button = (ImageButton) findViewById(R.id.item3ImageBtn);
+        item4Button = (ImageButton) findViewById(R.id.item4ImageBtn);
+        item5Button = (ImageButton) findViewById(R.id.item5ImageBtn);
         descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
-        final EditText searchEditText = (EditText) findViewById(R.id.searchEditText);
-        final ItemFactory itemFactory = new ItemFactory(this);
+        searchEditText = (EditText) findViewById(R.id.searchEditText);
+        itemFactory = new ItemFactory(this);
+        descriptionTextView.setMovementMethod(ScrollingMovementMethod.getInstance()); // allow scroll
+        textViewItem = new Item();
+        item = new Item();
+        for (int i = 0; i < 5; i++) {
+            equipments[i] = new Item();
+        }
 
-        heroImageButton.setBackgroundColor(5);
+        description = "";
+        itemHero = new Item();
+
         searchButton.setOnClickListener(new View.OnClickListener() {
-            Item textViewItem = new Item();
+
+
             @Override
             public void onClick(View view) {
+                int[] drawableIds = {R.drawable.p0, R.drawable.p1, R.drawable.p2, R.drawable.p3,
+                        R.drawable.p4, R.drawable.p5, R.drawable.p6, R.drawable.p7, R.drawable.p8,
+                        R.drawable.p9, R.drawable.p10, R.drawable.p11, R.drawable.p12, R.drawable.p13,
+                        R.drawable.p14, R.drawable.p15, R.drawable.p16, R.drawable.p17, R.drawable.p18,
+                        R.drawable.p19, R.drawable.p20, R.drawable.p21, R.drawable.p22, R.drawable.p23};
+
+                int[] drawableItemIds = {R.drawable.p100, R.drawable.p101, R.drawable.p102, R.drawable.p103,
+                        R.drawable.p104, R.drawable.p105, R.drawable.p106, R.drawable.p107, R.drawable.p108,
+                        R.drawable.p109, R.drawable.p110, R.drawable.p111, R.drawable.p112, R.drawable.p113,
+                        R.drawable.p114, R.drawable.p115,};
+
                 String text = searchEditText.getText().toString();
-                textViewItem = itemFactory.getItemByKeyword(text);
+                item = itemFactory.getItemByKeyword(text);
+                if (item.itemType == type.HERO && item.id != -1) {
+                    Toast.makeText(MainActivity.this, "hero添加", Toast.LENGTH_LONG).show();
+                    itemHero.id = item.id;
+                    itemHero.buff = item.buff;
+                    itemHero.description = item.description;
+                    itemHero.name = item.name;
+                    itemHero.picturePath = item.picturePath;
+                    //String s = itemHero.picturePath.substring
+                    //       (1,itemHero.picturePath.length()-1);
+                    heroImageButton.setImageResource(drawableIds[item.id]);
+                } else {
+                    for (int i = 0; i < 5; i++) {
+                        if (equipments[i].id == -1) {
+                            Toast.makeText(MainActivity.this, "第" + (i + 1) + "件装备", Toast.LENGTH_LONG).show();
+                            equipments[i].itemType = item.itemType;
+                            equipments[i].picturePath = item.picturePath;
+                            switch (i) {
+                                case 0:
+                                    item1Button.setImageResource(drawableItemIds[item.id - 100]);
+                                    break;
+                                case 1:
+                                    item2Button.setImageResource(drawableItemIds[item.id - 100]);
+                                    break;
+                                case 2:
+                                    item3Button.setImageResource(drawableItemIds[item.id - 100]);
+                                    break;
+                                case 3:
+                                    item4Button.setImageResource(drawableItemIds[item.id - 100]);
+                                    break;
+                                case 4:
+                                    item5Button.setImageResource(drawableItemIds[item.id - 100]);
+                                    break;
+                            }
+
+                            equipments[i].name = item.name;
+                            equipments[i].id = item.id;
+                            equipments[i].buff = item.buff;
+                            equipments[i].description = item.description;
+                            textViewItem.add(equipments[i]);
+                            break;
+
+                        }
+                        if (i == 4) {
+                            Toast.makeText(MainActivity.this, "英雄已神装！！！不能再添加", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+                /*
+                for(int i=0;i<5;i++){
+                    textViewItem.add(equipments[i]);
+                }
+                */
+
+                if (itemHero.id != -1) {
+                    textViewItem.add(itemHero);
+                    description = itemHero.description + "\n\n";
+                }
+                for (int i = 0; i < 5; i++) {
+                    if (equipments[i].id != -1) {
+                        description = description + equipments[i].name + "\n" + equipments[i].description + "\n";
+                    }
+                }
+
+
                 descriptionTextView.setText(
-                        "英雄名字：" + textViewItem.name + "\n"
+                        "英雄名字：" + itemHero.name + "\n"
                                 + "AD:" + textViewItem.buff.buff[0] + "\n"
                                 + "AP:" + textViewItem.buff.buff[1] + "\n"
                                 + "ADD:" + textViewItem.buff.buff[2] + "\n"
@@ -71,9 +164,9 @@ public class MainActivity extends AppCompatActivity
                                 + "MANA:" + textViewItem.buff.buff[4] + "\n"
                                 + "BLOOD:" + textViewItem.buff.buff[5] + "\n"
                                 + "PRICE:" + textViewItem.buff.buff[6] + "\n"
-                                + "DECRIPTION:" + textViewItem.description + "\n"
-                                + "ddddddd"
+                                + "DECRIPTION:" + description + "\n"
                 );
+                searchEditText.setText(null);
 
 
             }
